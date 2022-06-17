7ed569ec76d050eaae5b0b8809acc27397125e4b
Nicola Luigi Piriottu <88432636+NicolaPiriottu@users.noreply.github.com>
2022-06-17 15:11:38 +0200

    updated galleryResponse

--- src/response.js
+++ src/response.js
@@ -12,17 +12,23 @@ export const simpleResponse = {
 
 export const galleryResponse = {
   heading: {
-    title: "Titolo",
-    subtitle: "Sottotitolo"
+    title: "The Power of Now",
+    subtitle: "Learn to listen to people. You will listen to yourself."
   },
-  images: [
-    "https://img.freepik.com/premium-vector/hand-drawn-winter-landscape-with-mountains_23-2149190141.jpg?w=400",
-    "https://img.freepik.com/free-vector/beautiful-mountain-chain-landscape-sundown-with-birds_23-2148269223.jpg?w=400",
-    "https://img.freepik.com/free-vector/autumn-mountains-landscape-with-tree-silhouettes-river-sunset_148087-293.jpg?w=400"
-
-  ],
   body: {
     title: "Body Title",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  gallery: {
+    label: "My gallery",
+    images: [
+      "https://img.freepik.com/premium-vector/hand-drawn-winter-landscape-with-mountains_23-2149190141.jpg?w=400",
+      "https://img.freepik.com/free-vector/beautiful-mountain-chain-landscape-sundown-with-birds_23-2148269223.jpg?w=400",
+      "https://img.freepik.com/free-vector/autumn-mountains-landscape-with-tree-silhouettes-river-sunset_148087-293.jpg?w=400"
+  
+    ]
+  },
+  button: {
+    label: "Click Me!",
   }
 };
