60bd65cb9925204f0606090ee75d18557f3bb674
Nicola Luigi Piriottu <88432636+NicolaPiriottu@users.noreply.github.com>
2022-06-17 16:19:26 +0200

    load new image

--- src/response.js
+++ src/response.js
@@ -3,7 +3,7 @@ export const simpleResponse = {
     title: "The Power of Now",
     subtitle: "Learn to listen to people. You will listen to yourself."
   },
-  image: "https://img.freepik.com/premium-vector/hand-drawn-winter-landscape-with-mountains_23-2149190141.jpg?w=400",
+  image: "https://sites.google.com/site/gattiamici/_/rsrc/1468752127292/gatti-page/gatti%2001.jpg",
   body: {
     title: "Body Title",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
