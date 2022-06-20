e7d10745deb58caac2a19e1208685d471793303a
Nicola Luigi Piriottu <88432636+NicolaPiriottu@users.noreply.github.com>
2022-06-20 09:20:11 +0200

    updated response

--- src/response.js
+++ src/response.js
@@ -1,34 +1,37 @@
 export const simpleResponse = {
   heading: {
-    title: "The Power of Now",
-    subtitle: "Learn to listen to people. You will listen to yourself."
+    title: "Animals of the world",
+    subtitle: "Today we focus on:"
   },
   image: "https://sites.google.com/site/gattiamici/_/rsrc/1468752127292/gatti-page/gatti%2001.jpg",
   body: {
-    title: "Body Title",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    title: "Cats",
+    body: "The cat (Felis catus) is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family. A cat can either be a house cat, a farm cat or a feral cat; the latter ranges freely and avoids human contact. Domestic cats are valued by humans for companionship and their ability to kill rodents. About 60 cat breeds are recognized by various cat registries."
   }
 };
 
 export const galleryResponse = {
   heading: {
-    title: "The Power of Now",
-    subtitle: "Learn to listen to people. You will listen to yourself."
+    title: "Animals of the world",
+  },
+  sub: {
+    subtitle: "Today we focus on:"
   },
   body: {
-    title: "Body Title",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    title: "Cats",
+    partOne: "The cat (Felis catus) is a domestic species of small carnivorous mammal.",
+    partTwo: "It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family. A cat can either be a house cat, a farm cat or a feral cat; the latter ranges freely and avoids human contact. Domestic cats are valued by humans for companionship and their ability to kill rodents.",
+    partThree: "About 60 cat breeds are recognized by various cat registries."
+
   },
   gallery: {
-    label: "My gallery",
+    label: "Gallery",
     images: [
-      "https://img.freepik.com/premium-vector/hand-drawn-winter-landscape-with-mountains_23-2149190141.jpg?w=400",
-      "https://img.freepik.com/free-vector/beautiful-mountain-chain-landscape-sundown-with-birds_23-2148269223.jpg?w=400",
-      "https://img.freepik.com/free-vector/autumn-mountains-landscape-with-tree-silhouettes-river-sunset_148087-293.jpg?w=400"
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.humanesociety.org%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1240x698%2Fpublic%2F2020-07%2Fkitten-510651.jpg%3Fh%3Df54c7448%26itok%3DZhplzyJ9&imgrefurl=https%3A%2F%2Fwww.humanesociety.org%2Fresources%2Fcats-meow&tbnid=92QxTb8XPrhGhM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygZegUIARDmAQ..i&docid=LtlXi1psKWULWM&w=1240&h=698&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygZegUIARDmAQ",
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fe%2Fee%2FGrumpy_Cat_by_Gage_Skidmore.jpg&imgrefurl=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FGrumpy_Cat&tbnid=udAEBiJIe7jlJM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygUegUIARDWAQ..i&docid=0Bk_8vfTAt8b5M&w=1189&h=1548&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygUegUIARDWAQ",
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.nature.com%2Fw700%2Fmagazine-assets%2Fd41586-021-03080-7%2Fd41586-021-03080-7_19837260.jpg&imgrefurl=https%3A%2F%2Fwww.nature.com%2Farticles%2Fd41586-021-03080-7&tbnid=gM8cvFw6Y_eivM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyg-egQIARBN..i&docid=mbHNEjipUAs9AM&w=700&h=466&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyg-egQIARBN",
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.prestigeanimalhospital.com%2Fsites%2Fdefault%2Ffiles%2Finteresting-cat-facts.jpg&imgrefurl=https%3A%2F%2Fwww.prestigeanimalhospital.com%2Fservices%2Fcats%2Fblog%2F20-cat-facts-thatll-blow-your-kitty-crazed-mind&tbnid=Wjjl8FI5skNesM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyhJegQIARBk..i&docid=WM-XmpcVArxb_M&w=1024&h=427&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyhJegQIARBk"
   
     ]
-  },
-  button: {
-    label: "Click Me!",
   }
 };
