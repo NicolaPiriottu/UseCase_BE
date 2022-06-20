cafb600267050b30774a973696288e96584cd1c2
Nicola Luigi Piriottu <88432636+NicolaPiriottu@users.noreply.github.com>
2022-06-20 11:16:31 +0200

    temp

--- src/response.js
+++ src/response.js
@@ -27,6 +27,7 @@ export const galleryResponse = {
   gallery: {
     label: "Gallery",
     images: [
+      "https://sites.google.com/site/gattiamici/_/rsrc/1468752127292/gatti-page/gatti%2001.jpg",
       "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.humanesociety.org%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1240x698%2Fpublic%2F2020-07%2Fkitten-510651.jpg%3Fh%3Df54c7448%26itok%3DZhplzyJ9&imgrefurl=https%3A%2F%2Fwww.humanesociety.org%2Fresources%2Fcats-meow&tbnid=92QxTb8XPrhGhM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygZegUIARDmAQ..i&docid=LtlXi1psKWULWM&w=1240&h=698&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygZegUIARDmAQ",
       "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fe%2Fee%2FGrumpy_Cat_by_Gage_Skidmore.jpg&imgrefurl=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FGrumpy_Cat&tbnid=udAEBiJIe7jlJM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygUegUIARDWAQ..i&docid=0Bk_8vfTAt8b5M&w=1189&h=1548&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygUegUIARDWAQ",
       "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.nature.com%2Fw700%2Fmagazine-assets%2Fd41586-021-03080-7%2Fd41586-021-03080-7_19837260.jpg&imgrefurl=https%3A%2F%2Fwww.nature.com%2Farticles%2Fd41586-021-03080-7&tbnid=gM8cvFw6Y_eivM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyg-egQIARBN..i&docid=mbHNEjipUAs9AM&w=700&h=466&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyg-egQIARBN",
