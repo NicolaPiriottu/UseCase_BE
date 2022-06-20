88b4ea9ea637e64c72a04c2108fc85479eb4d000
Nicola Luigi Piriottu <88432636+NicolaPiriottu@users.noreply.github.com>
2022-06-20 11:22:36 +0200

    updated

--- src/response.js
+++ src/response.js
@@ -28,10 +28,9 @@ export const galleryResponse = {
     label: "Gallery",
     images: [
       "https://sites.google.com/site/gattiamici/_/rsrc/1468752127292/gatti-page/gatti%2001.jpg",
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.humanesociety.org%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1240x698%2Fpublic%2F2020-07%2Fkitten-510651.jpg%3Fh%3Df54c7448%26itok%3DZhplzyJ9&imgrefurl=https%3A%2F%2Fwww.humanesociety.org%2Fresources%2Fcats-meow&tbnid=92QxTb8XPrhGhM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygZegUIARDmAQ..i&docid=LtlXi1psKWULWM&w=1240&h=698&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygZegUIARDmAQ",
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fe%2Fee%2FGrumpy_Cat_by_Gage_Skidmore.jpg&imgrefurl=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FGrumpy_Cat&tbnid=udAEBiJIe7jlJM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygUegUIARDWAQ..i&docid=0Bk_8vfTAt8b5M&w=1189&h=1548&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMygUegUIARDWAQ",
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.nature.com%2Fw700%2Fmagazine-assets%2Fd41586-021-03080-7%2Fd41586-021-03080-7_19837260.jpg&imgrefurl=https%3A%2F%2Fwww.nature.com%2Farticles%2Fd41586-021-03080-7&tbnid=gM8cvFw6Y_eivM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyg-egQIARBN..i&docid=mbHNEjipUAs9AM&w=700&h=466&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyg-egQIARBN",
-      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.prestigeanimalhospital.com%2Fsites%2Fdefault%2Ffiles%2Finteresting-cat-facts.jpg&imgrefurl=https%3A%2F%2Fwww.prestigeanimalhospital.com%2Fservices%2Fcats%2Fblog%2F20-cat-facts-thatll-blow-your-kitty-crazed-mind&tbnid=Wjjl8FI5skNesM&vet=12ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyhJegQIARBk..i&docid=WM-XmpcVArxb_M&w=1024&h=427&q=cat&ved=2ahUKEwiajJvNvbv4AhXihM4BHTAjBHoQMyhJegQIARBk"
+      "https://sites.google.com/site/gattiamici/gatti/catus.JPG?attredirects=0",
+      "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Cat_paw_%28cloudzilla%29.jpg/180px-Cat_paw_%28cloudzilla%29.jpg",
+      "https://sites.google.com/site/gattiamici/cura-e-accessori/gatti%20access.jpg?attredirects=0"
   
     ]
   }
