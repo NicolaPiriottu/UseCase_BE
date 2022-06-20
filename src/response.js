6cd860695e12eadd31e486322fd79030d216b524
Nicola Luigi Piriottu <88432636+NicolaPiriottu@users.noreply.github.com>
2022-06-20 14:28:02 +0200

    update

--- src/response.js
+++ src/response.js
@@ -1,23 +1,26 @@
 export const simpleResponse = {
-  heading: {
+  title: {
     title: "Animals of the world",
     subtitle: "Today we focus on:"
   },
+
   image: "https://sites.google.com/site/gattiamici/_/rsrc/1468752127292/gatti-page/gatti%2001.jpg",
-  body: {
+
+  description: {
     title: "Cats",
     body: "The cat (Felis catus) is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family. A cat can either be a house cat, a farm cat or a feral cat; the latter ranges freely and avoids human contact. Domestic cats are valued by humans for companionship and their ability to kill rodents. About 60 cat breeds are recognized by various cat registries."
   }
 };
 
 export const galleryResponse = {
-  heading: {
+  title: {
     title: "Animals of the world",
   },
   sub: {
     subtitle: "Today we focus on:"
   },
-  body: {
+  
+  description: {
     title: "Cats",
     partOne: "The cat (Felis catus) is a domestic species of small carnivorous mammal.",
     partTwo: "It is the only domesticated species in the family Felidae and is often referred to as the domestic cat to distinguish it from the wild members of the family. A cat can either be a house cat, a farm cat or a feral cat; the latter ranges freely and avoids human contact. Domestic cats are valued by humans for companionship and their ability to kill rodents.",
